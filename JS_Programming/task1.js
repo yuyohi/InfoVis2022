70c60831d35a9fabf4b1e247942ced8c6023cb0e
yuyohi <41873114+yuyohi@users.noreply.github.com>
2022-04-13 13:52:07 +0900

    add test

--- JS_Programming/task1.js
+++ JS_Programming/task1.js
@@ -1,1 +1,18 @@
-console.log("test")
+class Vec3 {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  add = (v) => {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+
+    return this;
+  };
+
+  min = () => Math.min(this.x, this.y, this.z);
+
+}
