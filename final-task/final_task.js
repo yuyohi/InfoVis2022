16a0710909a5062b87a1a9db705bc4554e942a0f
yuyohi <41873114+yuyohi@users.noreply.github.com>
2022-06-10 03:14:21 +0900

    add death data

--- final-task/final_task.js
+++ final-task/final_task.js
@@ -1,4 +1,3 @@
-
 class Chart {
   constructor(config, data) {
     this.config = {
@@ -12,6 +11,16 @@ class Chart {
   }
 
   init() {
+    this.data = data.filter((v) => {
+      v.Year === "2019";
+    });
+
+    this.new_data = {};
+
+    this.data.foreach((v) => {
+      this.new_data[v.Entity] = Number(v.Deaths);
+    });
+
     this.svg = d3
       .select(this.config.parent)
       .attr("width", this.config.width)
@@ -19,10 +28,10 @@ class Chart {
 
     this.chart = this.svg
       .append("g")
-      //.attr(
-      //  "transform",
-      //  `translate(${this.config.margin.left}, ${this.config.margin.top})`,
-      //);
+      .attr(
+        "transform",
+        `translate(${this.config.margin.left}, ${this.config.margin.top})`,
+      );
 
     this.inner_width =
       this.config.width - this.config.margin.left - this.config.margin.right;
@@ -33,20 +42,36 @@ class Chart {
       .geoMercator()
       .center([0, 0]) //緯度経度の中心
       .translate([this.inner_width / 2, this.inner_height / 2]) //svgの中心
-      .scale(130); //地図の縮尺
+      .scale(100); //地図の縮尺
 
     const path = d3.geoPath().projection(projection);
 
-    d3.json("countries_geo.json").then((json) =>
-      chart
-        .selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("stroke", "dimgray")
-        .attr("stroke-width", 0.5)
-        .attr("fill", "lightgray"),
+    maxDeaths = d3.min(this.data, (d) => d.Deaths);
+    const color = d3
+      .scaleLinear()
+      .domain([0, maxDeaths])
+      .range(["white", "red"]);
+
+    d3.json("custom.geo.json").then(
+      (json) =>
+        this.chart
+          .selectAll("path")
+          .data(json.features)
+          .enter()
+          .append("path")
+          .attr("d", path)
+          .attr("fill", (d) => {
+            const deaths = this.new_data[d.properties.name];
+            return color(deaths);
+            //if (d.properties.name === "Japan") {
+            //  return "lightgray";
+            //} else {
+            //  return "666";
+            //}
+          })
+          .attr("stroke", "dimgray")
+          .attr("stroke-width", 0.5),
+      //.attr("fill", "lightgray"),
     );
   }
 
@@ -57,12 +82,14 @@ class Chart {
   render() {}
 }
 
-var config = {
-  parent: "#drawing_region",
-  width: 900,
-  height: 600,
-  margin: { top: 40, right: 40, bottom: 40, left: 40 },
-};
+d3.csv("./deaths-conflict-terrorism-per-100000.csv").then((data) => {
+  var config = {
+    parent: "#drawing_region",
+    width: 900,
+    height: 600,
+    margin: { top: 40, right: 40, bottom: 40, left: 40 },
+  };
 
-const graph = new Chart(config, "test");
-graph.update();
+  const graph = new Chart(config, data);
+  graph.update();
+});
